b61708a6804761f44657e72b62d3695fb1a03b51
AhmedKhanSalafi <kyrelpoll@gmail.com>
2024-09-26 16:54:24 +0530

    first java full stack project

---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-dark'>
+                <a className="navbar-brand" href="https://github.com/AhmedKhanSalafi">Ahmed Salafi Management System</a>
+                <a className="navbar-brand" href="https://youtu.be/nIbEnAJWc1A">shekar Management System</a>
+                <a className="navbar-brand" href="https://youtu.be/lvIs63DE6aw">srikant Management System</a>
+                
+                
+                
+            </nav>
+        </header>
+
+    </div>
+  )
+}
+
+export default HeaderComponent
